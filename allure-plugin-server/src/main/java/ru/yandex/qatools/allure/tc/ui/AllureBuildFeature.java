4d74d6e75dd2a68f86f0971382af2aa36a921935
Ivan Krutov <vania-pooh@yandex-team.ru>
2014-03-28 16:15:36 +0400

    Initial commit: plugin version that only generates Allure report and publishes it as build artifact.

---
+++ allure-plugin-server/src/main/java/ru/yandex/qatools/allure/tc/ui/AllureBuildFeature.java
@@ -0,0 +1,45 @@
+package ru.yandex.qatools.allure.tc.ui;
+
+import com.google.common.base.Strings;
+import jetbrains.buildServer.serverSide.BuildFeature;
+import jetbrains.buildServer.web.openapi.PluginDescriptor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import ru.yandex.qatools.allure.tc.Constants;
+
+import java.util.Map;
+
+public class AllureBuildFeature extends BuildFeature {
+    
+    private final PluginDescriptor pluginDescriptor;
+
+    public AllureBuildFeature(final PluginDescriptor pluginDescriptor) {
+        this.pluginDescriptor = pluginDescriptor;
+    }
+
+    @NotNull
+    @Override
+    public String getType() {
+        return Constants.BUILD_FEATURE_TYPE;
+    }
+
+    @NotNull
+    @Override
+    public String getDisplayName() {
+        return "Allure Report Build Feature";
+    }
+
+    @Nullable
+    @Override
+    public String getEditParametersUrl() {
+        return pluginDescriptor.getPluginResourcesPath("allureSettings.jsp");
+    }
+
+    @NotNull
+    @Override
+    public String describeParameters(@NotNull Map<String, String> params) {
+        final String inputDirectory = params.get(Constants.INPUT_DIRECTORY);
+        final String message = "<strong>Input files directory</strong> - path to directory with Allure input files relative to VCS root, e.g. <b>target/allure-results</b>";
+        return Strings.isNullOrEmpty(inputDirectory) ? message : message + "<br />Current input directory is <i>" + inputDirectory + "</i>";
+    }
+}
